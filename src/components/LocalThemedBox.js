e554efe7c0dc6038ae1b38cd16cc65b4511b616a
Harshit Verma <harshitverma1996@yahoo.com>
2024-01-04 14:30:15 +0530

    Update LocalThemedBox.js

--- src/components/LocalThemedBox.js
+++ src/components/LocalThemedBox.js
@@ -4,8 +4,9 @@ import React from 'react';
 const LocalThemedBox = () => {
 
 return(
-    <div style={{width:'200px',height:'200px',border:'2px solid green'}} id="local-themed-box">
+    <div style={{width:'200px',height:'200px',border:'2px solid green'}} id="local-themed-box" className={global?"bg-light":"bg-dark"}>
         {/* Write code below this line */}
+        <p id="local-themed-text-container"></p>
         
     </div>
 )
