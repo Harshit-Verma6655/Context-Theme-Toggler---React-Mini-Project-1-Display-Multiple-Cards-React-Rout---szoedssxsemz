cda3a36c40fc7616b2d357fb8ad8b04bddae77cb
Harshit Verma <harshitverma1996@yahoo.com>
2024-01-04 14:22:33 +0530

    Update ThemeToggleButton.js

--- src/components/ThemeToggleButton.js
+++ src/components/ThemeToggleButton.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 
 const ThemeToggleButton = () =>{
-    
+    let [global, setGlobal]=useState(true);
     return (
        <>
-        <button id='global-theme-toggler'>Switch to dark theme</button>
+        <button id='global-theme-toggler'>{global?"Switch to dark theme":"Switch to light theme'}</button>
         </>
     )
 
