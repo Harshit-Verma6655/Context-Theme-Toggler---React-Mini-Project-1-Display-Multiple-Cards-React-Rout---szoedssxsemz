c748eb3a23d53620c01bcccd2beef97ed9d94104
Harshit Verma <harshitverma1996@yahoo.com>
2024-01-04 14:34:49 +0530

    Update LocalThemedBox.js

--- src/components/LocalThemedBox.js
+++ src/components/LocalThemedBox.js
@@ -6,8 +6,9 @@ const LocalThemedBox = () => {
 return(
     <div style={{width:'200px',height:'200px',border:'2px solid green'}} id="local-themed-box" className={global?"bg-light":"bg-dark"}>
         {/* Write code below this line */}
-        <p id="local-themed-text-container"></p>
-        
+        <p id="local-themed-text-container" className={"text-light"}> lorem ipsum dolor iterit n stuff</p>
+            
+        <button id="local-theme-toggler" className={"btn btn-light"}>Toggle local theme to dark</button>
     </div>
 )
 }
